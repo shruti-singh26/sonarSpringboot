88428c6bb06e99b3721486aaa0d8111b72e51245
M1039304 <M1039304@A2ML23760.mindtree.com>
2018-10-04 17:47:38 +0530

    Manifest commit

---
+++ src/main/java/controllers/SpringbootWithJUnit.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringbootWithJUnit {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		SpringApplication.run(SpringbootWithJUnit.class,args);
+
+	}
+
+}
